82c6e2c5ce1010089c6ec057088c086b50334e13
gocjf0314 <gocjf12345@gmail.com>
2018-07-03 18:45:27 +0900

    secondcommit

---
+++ C programming 2/pointer-chapter12.c
@@ -0,0 +1,20 @@
+/*
+#include<stdio.h>
+int main(void) {
+int num1 = 10, num2 = 20;
+int *ptr1, *ptr2;
+ptr1 = &num1;
+ptr2 = &num2;
+
+(*ptr1) += 10;
+(*ptr2) -= 10;
+
+int *temp = ptr1;
+ptr1 = ptr2;
+ptr2 = temp;
+
+printf("ptr1: %d, ptr2; %d", *ptr1, *ptr2);
+
+return 0;
+}
+*/
